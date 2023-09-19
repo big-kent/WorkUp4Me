d58e2ab3ecf39539218a2889072789e86569fcc5
Big Kent <68211848+big-kent@users.noreply.github.com>
2023-09-19 17:32:32 +0700

    Initial Commit

---
+++ WorkUp4Me/WorkUp4MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkUp4MeApp.swift
+//  WorkUp4Me
+//
+//  Created by Khang Lu Minh on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkUp4MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
