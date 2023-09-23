0ff3f3adb696502f3569f5f1c3fa5fd2c0af7643
NhatVo2971 <s3868763@rmit.edu.vn>
2023-09-23 21:54:04 +0700

    update courseview

--- WorkUp4Me/ViewModels/CourseRelated/VideoDetailView.swift
+++ WorkUp4Me/ViewModels/CourseRelated/VideoDetailView.swift
@@ -8,13 +8,51 @@
 import SwiftUI
 
 struct VideoDetailView: View {
+    
+    var video: Video
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image(video.imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 150)
+                .cornerRadius(12)
+            
+            Text(video.title)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            HStack {
+                Label("\(video.viewCount)", systemImage: "eye.fill")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Text(video.uploadDate)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            Text(video.description)
+                .font(.body)
+                .padding()
+            Link(destination: video.url, label: {
+                Text("Watch Now")
+                    .bold()
+                    .font(.title2)
+                    .frame(width: 280, height: 50)
+                    .background(Color(.systemRed))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
+        }
     }
 }
 
 struct VideoDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        VideoDetailView()
+        VideoDetailView(video: VideoList.topTen.first!)
     }
 }
